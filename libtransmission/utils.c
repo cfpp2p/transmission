9f102d2f3e58e47bf5fb7a6d29303bdd77a109c9
cfpp2p <cfpp2p@computerfixpro.com>
2013-07-20 20:07:58 -0400

    fix 256 character problem

--- libtransmission/utils.c
+++ libtransmission/utils.c
@@ -1052,6 +1052,7 @@ tr_urlParse( const char * url_in,
             if( !err )
             {
                if( !strcmp( protocol, "udp" ) ) {
+                  if( n < 256 ) {
                   for( i=0; i<n; ++i ) {
                   // find next dot
                     if( pch[i] == 0x2E ) {
@@ -1060,11 +1061,13 @@ tr_urlParse( const char * url_in,
                     }
                     if( max_size_host_sub > 63 ) break;
                   }
-              if( i == n ) {
-                --i;
-                if( pch[i] != 0x2E ) max_size_host_sub = MAX( max_size_host_sub, (n - host_sub_start) );
-              }
+               if( i == n ) {
+                 --i;
+                 if( pch[i] != 0x2E ) max_size_host_sub = MAX( max_size_host_sub, (n - host_sub_start) );
+               }
               if( max_size_host_sub > 63 ) udp_will_fail = true;
+              }
+              else udp_will_fail = true;
              }
             } // End Ticket 5424 & 5426
 
